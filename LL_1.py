91a1fd9fa8d0703e39c44e770551d4a33fb4ed6d
darja-latisonoka-rtu <darja.latisonoka@edu.rtu.lv>
2025-02-11 07:07:35 +0000

    11.02

--- LL_1.py
+++ LL_1.py
@@ -23,15 +23,15 @@ class LinkedList:
         
 
     # WRITE FIND_MIDDLE_NODE METHOD HERE #
-def find_middle_node(self):
-    slow = self.head
-    fast = self.head
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
 
-    while fast is not None and fast.next is not None:
-        slow = slow.next
-        fast = fast.next.next
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
 
-    return slow
+        return slow
 
 
 
@@ -40,6 +40,7 @@ my_linked_list.append(2)
 my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
+my_linked_list.append(6)
 
 print( my_linked_list.find_middle_node().value )
 
